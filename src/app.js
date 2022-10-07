9aabaab1af8cb372a3d025da59392b60bbf48143
Denis Dolia <spda777@gmail.com>
2022-10-07 16:44:14 +0300

    third commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//script1
+let x = Number(promrt('первое число'));
+let y = Number(prompt('второе число'));
+
+if (x>y) {
+    document.write('x > y');
+} else if (x<y) {
+    document.write('x < y');
+} else if (x===y) {
+    document.write('x = y');
+}
+
+//script 2 
