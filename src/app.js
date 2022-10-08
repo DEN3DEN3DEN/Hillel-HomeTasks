3979067b15061326cff55f063ec4fd278d5649a4
Denis Dolia <spda777@gmail.com>
2022-10-08 22:34:27 +0300

    script1-8

--- src/app.js
+++ src/app.js
@@ -90,5 +90,18 @@ if (digitA === digitB && digitA === digitC) {
 }*/
 
 //script 8
+let numberABCDEF = Number(prompt('введите шестизначное число'));
+let digitA = parseInt(numberABCDEF / 100000);
+let digitB = parseInt((numberABCDEF / 10000) % 10);
+let digitC = parseInt((numberABCDEF / 1000) % 10);
+let digitD = parseInt((numberABCDEF / 100) % 10);
+let digitE = parseInt((numberABCDEF / 10) % 10);
+let digitF = parseInt(numberABCDEF % 10);
+
+if (digitA===digitF && digitB===digitE && digitC===digitD) {
+    console.log('это число зеркальное');
+} else {
+    console.log('обычное шестизначное число');
+}
 
 
