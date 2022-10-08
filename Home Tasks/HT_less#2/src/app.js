733994f5383dbb0a5823a6ad66444a3c85ceee1f
Dolia Denis <102805085+DEN3DEN3DEN@users.noreply.github.com>
2022-10-08 22:00:48 +0300

    1323

---
+++ Home Tasks/HT_less#2/src/app.js
@@ -0,0 +1,19 @@
+/*Первое задание
+let userName = prompt("what is your name?");
+alert('Hello, ' + userName + '! How are you?');*/
+
+//второе задание
+let a = Number(prompt(' Введите число:','0'));
+let b = Number(prompt(' Введите число:','0'));
+alert([a] + '+' + [b] + '=' + [a+b]);
+alert([a] + '-' + [b] + '=' + [a-b]);
+alert([a] + '*' + [b] + '=' + [a*b]);
+alert([a] + '/' + [b] + '=' + [a/b]);
+
+
+/*третье задание
+let a = parseInt(prompt(' Введите число:','0'));
+let b = parseInt(prompt(' Введите число:','0'));
+let c = parseInt(prompt(' Введите число:','0'));
+let average = (a+b+c)/3;
+alert(average);*/
