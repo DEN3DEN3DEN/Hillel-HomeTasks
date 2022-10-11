86cc1e2c40e658f5e752367a9f6eff9b717d58a1
Denis Dolia <spda777@gmail.com>
2022-10-11 20:28:01 +0300

    Final

--- src/app2.js
+++ src/app2.js
@@ -38,36 +38,12 @@ let sportsman3 = 'Ефим Конопля';
 
 
 if (yearOfBirth > 0) {
-    alert('Твой возраст - ' + [age] + '!');
-} else if (yearOfBirth === 0) {
-    alert('Жаль, что вы не захотели ввести свой возрвст!');
+    console.log('Твой возраст - ' + [age] + '!');
+} else if (yearOfBirth === null) {
+    console.log('Жаль, что вы не захотели ввести свой возрвст!');
 }
 
-if (city === city1) {
-    alert('Ты живешь в столице ' + [country1] + '!');
-} else if (city === city2) {
-    alert('Ты живешь в столице ' + [country2] + '!');
-} else if (city === city3) {
-    alert('Ты живешь в столице ' + [country3] + '!');
-} else if (city) {
-    alert('Ты живешь в городе ' + [city] + '!');
-} else {
-    alert('Жаль, что вы не захотели ввести свой город!');
-}
-
-if (sport === sport1) {
-    alert('Круто! Хочешь стать как ' + [sportsman1] + '?');
-} else if (sport === sport2) {
-    alert('Круто! Хочешь стать как ' + [sportsman2] + '?');
-} else if (sport === sport3) {
-    alert('Круто! Хочешь стать как ' + [sportsman3] + '?');
-} else if (sport) {
-    alert('Твой любимый спорт ' + [sport] + '!');
-} else {
-    alert('Жаль, что вы не захотели ввести свой любимый вид спорта!');
-}
-
-/*switch (city) {
+switch (city) {
     case city1:
         CountryAlert(country1)
         break;
@@ -78,14 +54,14 @@ if (sport === sport1) {
         CountryAlert(country3)
         break;
     case null:
-        alert ('Жаль, что вы не захотели ввести свой город');
+        console.log('Жаль, что вы не захотели ввести свой город!');
         break;
     default:
-        alert ('Ты живешь в городе ' + [city]);
+        console.log('Ты живешь в городе ' + [city]);
 }
 
 function CountryAlert(country) {  
-    alert('Ты живешь в столице ' + [country]);
+    console.log('Ты живешь в столице ' + [country]);
 }
 
 switch (sport) {
@@ -93,18 +69,24 @@ switch (sport) {
         sportsmanAlert(sportsman1)
         break;
     case sport2:
-        sportsmanalert(sportsman2)
+        sportsmanAlert(sportsman2)
         break;
     case sport3:
-        sportsmanalert(sportsman3)
+        sportsmanAlert(sportsman3)
         break;
     case null:
-        alert ('Жаль, что вы не захотели ввести свой любимый вид спорта');
+        console.log('Жаль, что вы не захотели ввести свой любимый вид спорта!');
         break;
     default:
-        alert ('Твой любимый вид спорта ' + [sport]);
+        console.log('Твой любимый вид спорта ' + [sport]);
 }
 
 function sportsmanAlert(sportsman) {  
-    alert('Круто! Хочешь стать как ' + [sportsman]);
-}*/
+    console.log('Круто! Хочешь стать как ' + [sportsman]);
+}
+
+let ageStr = 'Твой возраст - ' + [age] + '!';
+let cityStr = 'Ты живешь в городе ' + [city] + '!';
+let sportStr = 'Твой любимый вид спорта ' + [sport] + '!';
+
+alert(ageStr+' '+cityStr+' '+sportStr);
