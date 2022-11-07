c658a74f9a0604f0c29d53317f24196485827c07
Dolia Denis <102805085+DEN3DEN3DEN@users.noreply.github.com>
2022-11-07 13:49:33 +0200

    Delete HT_less#9/App3 directory

--- HT_less#9/App3/app.js
+++
@@ -1,8 +0,0 @@
-// В папке images есть изображения 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg. Вывести изображение из этой папки полученное случайным образом (Math.random)
-
-
-const arr = ["image1.jpg", "image2.jpg", "image3.jpg"];
-function change(){
-   const i = Math.floor(Math.random() * arr.length);
-   document.getElementById("myimg").src = arr[i];
-}
